7189b2651b32f96039078c42d32eb5ab3bb66af0
KHegedus1 <kico.kiki@gmail.com>
2022-06-24 22:34:13 +0200

    pridruzivanje vrijednosti iz jedne varijable u drugu

--- ponavljanje.js
+++ ponavljanje.js
@@ -7,5 +7,15 @@ var imeVarijable; // Ne smiju imati razmake niti pocinjati s brojem.
 
 imeVarijable=5;
 
-var assigmentOperator = 5; //
+var assigmentOperator = 5; 
+
+
+
+var prva;
+
+prva = 3;
+
+var druga;
+
+druga = prva;
 
