8303087f424ea450e8eaa3305b21f85c7dc08fb7
KHegedus1 <kico.kiki@gmail.com>
2022-06-24 22:43:47 +0200

    pridruzivanje stringova,undefined

--- ponavljanje.js
+++ ponavljanje.js
@@ -22,3 +22,16 @@ druga = prva; //mora ici druga pa prva
 var myFirstName = "Qrsars";
 var myLastName = "Asfsfs";
 
+var a;
+var b;
+var c;
+
+a= 5;
+b= 10;
+c= "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!"; // varijable imaju pocetnu vrijednost undefined
+
+
+
