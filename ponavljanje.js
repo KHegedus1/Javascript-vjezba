7b2e099b78899d0153a3683dc33844ee4ce23f55
KHegedus1 <kico.kiki@gmail.com>
2022-06-26 23:15:32 +0200

    pozivanje varijable u pridruzivanju

--- ponavljanje.js
+++ ponavljanje.js
@@ -98,3 +98,6 @@ const myStr = "This is the start." + "This is the end.";
 
 let myStr = "This is the first sentence."
 myStr +=" This is the second sentence."
+
+const myName = "Kristijan";
+const myStr = "My name is" + myName +"and I am well!";
