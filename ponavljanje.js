8eb1441d7676cdaf045ce2d9470e506ae24085e4
KHegedus1 <kico.kiki@gmail.com>
2022-06-28 23:52:21 +0200

    inequality !=

--- ponavljanje.js
+++ ponavljanje.js
@@ -304,3 +304,13 @@ function nextInLine(arr, item) {
     }
     
     compareEquality(10, "10");
+
+    
+function testNotEqual(val) {
+  if (val !=99) {
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
