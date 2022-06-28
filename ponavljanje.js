e8219d401db899f841d31eb8b238d0778e2bc2f0
KHegedus1 <kico.kiki@gmail.com>
2022-06-29 00:05:45 +0200

    else

--- ponavljanje.js
+++ ponavljanje.js
@@ -404,3 +404,17 @@ function testLogicalOr(val) {
 }
 
 testLogicalOr(15);
+
+function testElse(val) {
+  let result = "";
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }else{
+    result = "5 or Smaller";
+  }
+
+  return result;
+}
+
+testElse(4);
