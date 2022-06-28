5694b295332a51e681c96c245bb7e3cee0bae8f7
KHegedus1 <kico.kiki@gmail.com>
2022-06-29 00:02:45 +0200

    ||

--- ponavljanje.js
+++ ponavljanje.js
@@ -391,3 +391,16 @@ function testLogicalAnd(val) {
 }
 
 testLogicalAnd(10);
+
+function testLogicalOr(val) {
+
+  if (val >20 || val <10) {
+    return "Outside";
+  }
+
+
+
+  return "Inside";
+}
+
+testLogicalOr(15);
