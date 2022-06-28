7a8ee2d996a9e17eb862582b306dd1af8ad7707a
KHegedus1 <kico.kiki@gmail.com>
2022-06-28 23:53:01 +0200

    strict inequality !==

--- ponavljanje.js
+++ ponavljanje.js
@@ -314,3 +314,13 @@ function testNotEqual(val) {
 }
 
 testNotEqual(10);
+
+
+function testStrictNotEqual(val) {
+  if (val !== 17) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
