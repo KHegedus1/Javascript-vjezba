15bae8a74ce7785e9d599eacfd19c2fe4a2c1b96
KHegedus1 <kico.kiki@gmail.com>
2022-06-29 00:43:10 +0200

    default switch

--- ponavljanje.js
+++ ponavljanje.js
@@ -499,3 +499,24 @@ function caseInSwitch(val) {
   return answer;
 }
 caseInSwitch(1);
+
+function switchOfStuff(val) {
+  var answer = "";
+
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+  }
+
+  return answer;
+}
+
