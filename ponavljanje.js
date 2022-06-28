69b9835f7d52806c1a201ba67c3e05a7079160fc
KHegedus1 <kico.kiki@gmail.com>
2022-06-29 00:15:10 +0200

    chaining

--- ponavljanje.js
+++ ponavljanje.js
@@ -442,3 +442,18 @@ function orderMyLogic(val) {
 }
 
 orderMyLogic(7);
+
+function testSize(num) {
+  if(num <5){
+    return "Tiny";
+  }else if(num <10){
+    return "Small";
+  }else if(num <15){
+    return "Medium";
+  }else if(num <20){
+    return "Large";
+  }else
+  return "Huge";
+}
+
+testSize(7);
