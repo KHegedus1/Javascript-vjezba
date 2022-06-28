d529efd4c7f1dc62ae6cb859112fdaccd6aeb5fe
KHegedus1 <kico.kiki@gmail.com>
2022-06-28 23:47:44 +0200

    if statements

--- ponavljanje.js
+++ ponavljanje.js
@@ -270,3 +270,10 @@ function nextInLine(arr, item) {
     return true; 
   
   }
+
+  function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue){
+      return "Yes, that was true"
+    }
+    return "No, that was false"
+    }
