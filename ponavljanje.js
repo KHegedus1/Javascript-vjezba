f856a214cb2f0b49f2cff702a42890dc0b0bc837
KHegedus1 <kico.kiki@gmail.com>
2022-06-30 17:47:57 +0200

    pretvaranje if/if else u switch

--- ponavljanje.js
+++ ponavljanje.js
@@ -542,3 +542,33 @@ function sequentialSizes(val) {
 }
 
 sequentialSizes(1);
+
+function chainToSwitch(val) {
+  let answer = "";
+
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42: 
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+    case "John":
+      answer = "";
+      break;
+    case 156:
+      answer = "";
+  }
+  return answer;
+}
+
+chainToSwitch(7);
