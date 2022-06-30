2f2fdbafa0fcd5a37529798dbf8d162459302b96
KHegedus1 <kico.kiki@gmail.com>
2022-06-30 18:26:46 +0200

    dodavanje svojstva u objekt

--- ponavljanje.js
+++ ponavljanje.js
@@ -653,3 +653,11 @@ const myDog = {
 };
 
 myDog.name = "Happy Coder";
+
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.bark = "woof";
