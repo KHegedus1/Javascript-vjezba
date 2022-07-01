962d8d0388e4001405ba1146244a8947f69fc816
KHegedus1 <kico.kiki@gmail.com>
2022-07-01 23:43:34 +0200

    pristup ugnijezdjenim objektima

--- ponavljanje.js
+++ ponavljanje.js
@@ -715,3 +715,16 @@ const myMusic = [
     "formats": [ "CD","8T", "LP" ],
   }
 ];
+
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+const gloveBoxContents = myStorage.car.inside["glove box"];
