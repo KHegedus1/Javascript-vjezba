35c0ce2daa9f0efe9eb3b8bcfaebe4e7c96b775f
KHegedus1 <kico.kiki@gmail.com>
2022-07-04 23:57:27 +0200

    ternary operator

--- ponavljanje.js
+++ ponavljanje.js
@@ -919,3 +919,10 @@ function randomWholeNum() {
     return parseInt(str, 2);
   }
   convertToInteger("10011");
+
+  function checkEqual(a, b) {
+    return a == b ? "Equal" : "Not Equal";
+  
+  }
+  
+  checkEqual(1, 2);
