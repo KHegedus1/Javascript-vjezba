6215f5ad4715a606ce6803a329720b653259e182
KHegedus1 <kico.kiki@gmail.com>
2022-07-08 14:33:47 +0200

    getters and setters

--- ES6.js
+++ ES6.js
@@ -140,3 +140,22 @@ class Vegetable {
 
 const carrot = new Vegetable('carrot');
 console.log(carrot.name); 
+
+class Thermostat {
+  constructor(F) {
+  this.F = F;
+    }
+    
+  get temperature() {
+  return (5 / 9) * (this.F - 32);
+    }
+    
+  set temperature(C) {
+  this.F = (C * 9.0) / 5 + 32;
+    }
+  }
+  
+  const thermos = new Thermostat(76);
+  let temp = thermos.temperature; 
+  thermos.temperature = 26;
+  temp = thermos.temperature; 
