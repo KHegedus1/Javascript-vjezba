e43fc3602ac705f6df2015e6a03f2521ad403af9
KHegedus1 <kico.kiki@gmail.com>
2022-07-09 22:59:09 +0200

    then

--- ES6.js
+++ ES6.js
@@ -208,3 +208,16 @@ const makeServerRequest = new Promise((resolve, reject) => {
     reject("Data not received");
   }
 });
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  let responseFromServer = true;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+  makeServerRequest.then(result =>{
+    console.log(result);
+  });
+});
