276e42af990deed0acc10502584063d591cc5447
KHegedus1 <kico.kiki@gmail.com>
2022-07-09 22:46:58 +0200

    import * as-import svega iz druge datoteke

--- ES6.js
+++ ES6.js
@@ -179,3 +179,10 @@ import { lowercaseString } from './string_functions.js';
 
 uppercaseString("hello");
 lowercaseString("WORLD!");
+
+
+import * as stringFunctions from "./string_functions.js";
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
