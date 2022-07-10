8477ec3e58fc20432559ce3ae3c46deb26aa5953
KHegedus1 <kico.kiki@gmail.com>
2022-07-10 23:27:43 +0200

    Match Literal Strings

--- regularExpressions.js
+++ regularExpressions.js
@@ -2,3 +2,6 @@ let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
 
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/;
+let result = waldoRegex.test(waldoIsHiding);
