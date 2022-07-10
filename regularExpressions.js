73a6489c391a02f7b86afa9821174caf4847819b
KHegedus1 <kico.kiki@gmail.com>
2022-07-10 23:29:08 +0200

    Match a Literal String with Different Possibilities

--- regularExpressions.js
+++ regularExpressions.js
@@ -5,3 +5,7 @@ let result = myRegex.test(myString);
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/;
 let result = waldoRegex.test(waldoIsHiding);
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; 
+let result = petRegex.test(petString);
