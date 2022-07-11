05cb7db5a1192d5dbbb99b51e4ba679494ab0a37
KHegedus1 <kico.kiki@gmail.com>
2022-07-11 23:21:29 +0200

    Update regularExpressions.js

--- regularExpressions.js
+++ regularExpressions.js
@@ -33,3 +33,4 @@ let result = quoteSample.match(vowelRegex);
 let quoteSample = "The quick brown fox jumps over the lazy dog.";
 let alphabetRegex = /[a-z]/gi;
 let result = quoteSample.match(alphabetRegex);
+
