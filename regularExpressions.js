3379349625de693a6b966c467bf5912f9e0c9cb5
KHegedus1 <kico.kiki@gmail.com>
2022-07-12 22:42:11 +0200

    Trazi sve osim brojeva

--- regularExpressions.js
+++ regularExpressions.js
@@ -74,3 +74,7 @@ let result = quoteSample.match(nonAlphabetRegex).length;
 let movieName = "2001: A Space Odyssey";
 let numRegex = /\d/gi; 
 let result = movieName.match(numRegex).length;
+
+let movieName = "2001: A Space Odyssey";
+let noNumRegex = /\D/gi; 
+let result = movieName.match(noNumRegex).length;
