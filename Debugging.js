f0ea20dfc701008c6092429a13d8444f93017681
KHegedus1 <kico.kiki@gmail.com>
2022-07-14 23:42:01 +0200

    buggovi pri indeksiranju

--- Debugging.js
+++ Debugging.js
@@ -71,4 +71,15 @@ function getNine() {
 
 
 
+  function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    for (let i = 0; i < len; i++) {
+      console.log(firstFive[i]);
+    }
+  }
+  
+  countToFive();
+
+
   
