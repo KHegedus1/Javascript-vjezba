2b1af24de80d9288be8ce0d14cd4c89771ec3544
KHegedus1 <kico.kiki@gmail.com>
2022-07-14 23:37:19 +0200

    popravak redoslijeda argumenata u funkciji

--- Debugging.js
+++ Debugging.js
@@ -57,4 +57,18 @@ function getNine() {
   let result = getNine();
   console.log(result);
 
+
+
+
+  function raiseToPower(b,e) {
+    return Math.pow(b,e);
+  }
+  
+  let base = 2;
+  let exp = 3;
+  let power = raiseToPower(base, exp);
+  console.log(power);
+
+
+
   
