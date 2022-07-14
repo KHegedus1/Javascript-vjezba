f53e3b4074da4e187befaf775396b374988e7250
KHegedus1 <kico.kiki@gmail.com>
2022-07-14 23:29:34 +0200

    jednostruki navodnici u dvorstrukim navodnicima

--- Debugging.js
+++ Debugging.js
@@ -29,3 +29,9 @@ console.log(`Net working capital is: ${netWorkingCapital}`);
 let myArray = [1, 2, 3];
 let arraySum = myArray.reduce(((previous, current) =>  previous + current));
 console.log(`Sum of array values is: ${arraySum}`);
+
+
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml);
+
+
