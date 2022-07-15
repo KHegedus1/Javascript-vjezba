6d39bd26a94a87eccf478c95a2600c6694aaff8b
KHegedus1 <kico.kiki@gmail.com>
2022-07-15 22:18:07 +0200

    pop, shift

--- BasicDataStructures.js
+++ BasicDataStructures.js
@@ -5,6 +5,7 @@ let myArray = ["a", "b", "c", "d"];
 myArray[1] = "Nova vrijednost";
 console.log(myArray);
 
+
 function mixedNumbers(arr) {
     arr.unshift('I',2,'three');
     arr.push(7,'VIII',9);
@@ -14,6 +15,14 @@ function mixedNumbers(arr) {
   
   console.log(mixedNumbers(['IV', 5, 'six']));
 
+
+  function popShift(arr) {
+    let popped = arr.pop(); 
+    let shifted =arr.shift(); 
+    return [shifted, popped];
+  }
+  
+  console.log(popShift(['challenge', 'is', 'not', 'complete']));
 
 
   
