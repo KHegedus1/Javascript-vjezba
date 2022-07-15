fa3b54b69f63c07fb1b04ea93a27d9533d6ef0a0
KHegedus1 <kico.kiki@gmail.com>
2022-07-15 22:10:22 +0200

    Promjena vrijednosti iz arraya putem indexa

--- BasicDataStructures.js
+++ BasicDataStructures.js
@@ -1,3 +1,6 @@
 let yourArray = ['one', 2, 'three', true, false, undefined, null];
 console.log(yourArray.length);
 
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = "Nova vrijednost";
+console.log(myArray);
