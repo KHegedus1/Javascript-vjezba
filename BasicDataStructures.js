b81a998172e4b52fd76437c5c03d9d57cd5fc5fa
KHegedus1 <kico.kiki@gmail.com>
2022-07-16 22:34:26 +0200

    indexof

--- BasicDataStructures.js
+++ BasicDataStructures.js
@@ -66,4 +66,13 @@ function htmlColorNames(arr) {
   
 
   
+  function quickCheck(arr, elem) {
+    if (arr.indexOf(elem) >= 0)  {
+      return true;
+    }
+    return false;
+  }
+  console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
+
+
   
