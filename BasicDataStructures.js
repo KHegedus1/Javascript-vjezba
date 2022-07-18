8ea19d5c4b1b9b495f5652ec0f3a316f517c466c
KHegedus1 <kico.kiki@gmail.com>
2022-07-18 22:52:40 +0200

    Access Property Names with Bracket Notation

--- BasicDataStructures.js
+++ BasicDataStructures.js
@@ -125,3 +125,20 @@ userActivity.data.online = 45;
 console.log(userActivity);
 
 
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+  return foods[scannedItem];
+
+}
+
+console.log(checkInventory("apples"));
+
+
