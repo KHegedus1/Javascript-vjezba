731766c0a9e65784f255cb663f77644e74119504
KHegedus1 <kico.kiki@gmail.com>
2022-07-18 22:51:44 +0200

    Update BasicDataStructures.js

--- BasicDataStructures.js
+++ BasicDataStructures.js
@@ -111,3 +111,17 @@ foods["strawberries"] = 27;
 console.log(foods);
 
 
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+
+userActivity.data.online = 45;
+
+console.log(userActivity);
+
+
