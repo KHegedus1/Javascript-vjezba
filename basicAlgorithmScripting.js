8554a32f23f5c56344b018dcd4f9dae26392d8c3
KHegedus1 <kico.kiki@gmail.com>
2022-07-19 23:26:49 +0200

    trazenje najvecih brojeva u arrayima

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -34,3 +34,13 @@ function reverseString(str) {
       }
       
       findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+
+      function largestOfFour(arr) {
+        return arr.map(Function.apply.bind(Math.max, null));
+      }
+      
+      largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+      
