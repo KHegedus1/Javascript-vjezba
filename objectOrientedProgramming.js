c45be6bb42a725f2176144e30d9c8f89174541ed
KHegedus1 <kico.kiki@gmail.com>
2022-07-21 23:49:19 +0200

    this.

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -23,4 +23,15 @@
       dog.sayLegs();
 
 
+      let dog = {
+        name: "Spot",
+        numLegs: 4,
+        sayLegs: function() {
+          return "This dog has " + this.numLegs + " legs.";
+        }
+      };
+      
+      dog.sayLegs();
+
+
       
