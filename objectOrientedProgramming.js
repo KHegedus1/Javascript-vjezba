1336d1e8d7cd0a775e46b51c5b49cb7e86b49524
KHegedus1 <kico.kiki@gmail.com>
2022-07-21 23:45:07 +0200

    dot notation

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -1,7 +1,15 @@
-let dog = {
+    let dog = {
     name :"Pas",
     numLegs : 4
     };
 
 
+    let dog = {
+        name: "Spot",
+        numLegs: 4
+      };
+      console.log(dog.name);
+      console.log(dog.numLegs);
 
+
+    
