6c86edb56930879ed0c530ed221fdf92b1207987
KHegedus1 <kico.kiki@gmail.com>
2022-07-21 23:48:31 +0200

    metoda na objektu

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -12,4 +12,15 @@
       console.log(dog.numLegs);
 
 
-    
+      let dog = {
+        name: "Spot",
+        numLegs: 4,
+        sayLegs: function() {
+          return "This dog has " + dog.numLegs + " legs.";
+        }
+      };
+      
+      dog.sayLegs();
+
+
+      
