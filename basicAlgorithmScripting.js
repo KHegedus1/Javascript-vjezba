52344035e5e33187a17da3b53327e00d163fe63c
KHegedus1 <kico.kiki@gmail.com>
2022-07-21 23:40:02 +0200

    chunky arrays

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -121,4 +121,13 @@ function reverseString(str) {
       }
 
       
+      function chunkArrayInGroups(arr, size) {
+        if (arr.length <= size) {
+          return [arr];
+        } else {
+          return [arr.slice(0, size)].concat(
+            chunkArrayInGroups(arr.slice(size), size)
+          );
+        }
+      }
       
