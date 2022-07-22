07665cd84aadfb7a0f579bfa9ac45259027ea9f9
KHegedus1 <kico.kiki@gmail.com>
2022-07-22 23:22:53 +0200

    isPrototypeOf

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -156,4 +156,10 @@
 
 
 
+        function Dog(name) {
+          this.name = name;
+        }
         
+        let beagle = new Dog("Snoopy");
+        
+        Dog.prototype.isPrototypeOf(beagle);
