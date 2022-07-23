324e1059672ed3baf520e63bfa09a9e83866dea4
KHegedus1 <kico.kiki@gmail.com>
2022-07-23 13:58:44 +0200

    object prototype

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -163,3 +163,18 @@
         let beagle = new Dog("Snoopy");
         
         Dog.prototype.isPrototypeOf(beagle);
+
+
+        function Dog(name) {
+          this.name = name;
+        }
+        
+        let beagle = new Dog("Snoopy");
+        
+        Dog.prototype.isPrototypeOf(beagle);  // yields true
+        
+        Object.prototype.isPrototypeOf(Dog.prototype);
+
+        
+
+        
