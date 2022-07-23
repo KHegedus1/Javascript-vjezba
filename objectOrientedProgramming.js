48b5ecd7858ad2faa43ca46be67f70c8bd4ffaad
KHegedus1 <kico.kiki@gmail.com>
2022-07-23 14:06:24 +0200

    spajanje podtipa sa supertipom

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -219,3 +219,18 @@ let beagle = Object.create(Animal.prototype);
 
 
 
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+Dog.prototype = Object.create(Animal.prototype);
+let beagle = new Dog();
+
+
+
