c9dbefcdbead73f6e226dfaa7ff544c6070c92a1
KHegedus1 <kico.kiki@gmail.com>
2022-07-23 14:21:13 +0200

    IIFE za module

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -316,3 +316,20 @@ function Bird() {
 
 
 
+let funModule = (function() {
+  return {
+    isCuteMixin: function(obj) {
+      obj.isCute = function() {
+        return true;
+      };
+    },
+    singMixin: function(obj) {
+      obj.sing = function() {
+        console.log("Singing to an awesome tune");
+      };
+    }
+  };
+})();
+
+
+
