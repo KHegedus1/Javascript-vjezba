51fddd62286d5e79b65e6ecee10a281b8d34f742
KHegedus1 <kico.kiki@gmail.com>
2022-07-23 14:16:17 +0200

    Mixin

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -280,3 +280,23 @@ console.log(penguin.fly());
 
 
 
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+
+let boat = {
+  name: "Warrior",
+  type: "race-boat"
+};
+
+let glideMixin = function(obj) {
+  obj.glide = function() {
+    console.log("Gliding!");
+  };
+};
+glideMixin(bird);
+glideMixin(boat);
+
+
+
