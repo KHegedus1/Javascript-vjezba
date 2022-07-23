bdaf523cb8aff6f4cdf32217f3c6003ecb66bb22
KHegedus1 <kico.kiki@gmail.com>
2022-07-23 14:04:37 +0200

    inherit

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -204,4 +204,18 @@
         };
 
 
-        
+        function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+
+let duck = Object.create(Animal.prototype);
+let beagle = Object.create(Animal.prototype);
+
+
+
