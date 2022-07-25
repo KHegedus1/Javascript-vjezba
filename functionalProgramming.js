130fa5d020447d94835283e7571ec2361e3cb299
KHegedus1 <kico.kiki@gmail.com>
2022-07-25 20:22:02 +0200

    map implementation

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -232,3 +232,19 @@ console.log(JSON.stringify(ratings));
 
 
 
+const s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i]));
+  }
+  return newArray;
+};
+
+const new_s = s.myMap(function(item) {
+  return item * 2;
+});
+
+
+
