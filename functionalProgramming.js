9d657a220b327ca19cad202efb86d03493a93a9f
KHegedus1 <kico.kiki@gmail.com>
2022-07-27 17:56:17 +0200

    concat umjesto push

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -422,4 +422,15 @@ function nonMutatingConcat(original, attach) {
 
 
 
- 
+ function nonMutatingPush(original, newItem) {
+
+  return original.concat(newItem);
+
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingPush(first, second);
+
+
+
