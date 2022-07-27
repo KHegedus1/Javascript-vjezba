50ce9bd2e559439ba09ecabef7d7b6d2f831d8cf
KHegedus1 <kico.kiki@gmail.com>
2022-07-27 17:55:28 +0200

    concat

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -412,3 +412,14 @@ nonMutatingSplice(inputCities);
 
 
 
+function nonMutatingConcat(original, attach) {
+  return original.concat(attach);
+ }
+ 
+ const first = [1, 2, 3];
+ const second = [4, 5];
+ nonMutatingConcat(first, second);
+
+
+
+ 
