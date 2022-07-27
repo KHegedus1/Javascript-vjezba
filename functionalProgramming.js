a7ae6b60caadadf221e5f7e6cd31de51d436e67b
KHegedus1 <kico.kiki@gmail.com>
2022-07-27 18:08:36 +0200

    sort vjezba

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -581,3 +581,13 @@ alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
 
 
 
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  return [].concat(arr).sort(function(a, b) {
+    return a - b;
+  });
+}
+nonMutatingSort(globalArray);
+
+
+
