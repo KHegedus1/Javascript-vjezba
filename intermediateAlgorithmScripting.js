a1054f52e3bc30a3e75d3d0304e22a0f7a2e6913
KHegedus1 <kico.kiki@gmail.com>
2022-07-28 15:27:58 +0200

    DNA Pairing vjezba

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -73,4 +73,30 @@ function sumAll(arr) {
 
   
 
-  
+  function pairElement(str) {
+    const paired = [];
+      for (let i = 0; i < str.length; i++) {
+        matchWithBasePair(str[i], paired);
+      }
+    
+      return paired;
+    }
+      let matchWithBasePair = function(char, pairedArray) {
+        switch (char) {
+          case "A":
+            pairedArray.push(["A", "T"]);
+            break;
+          case "T":
+            pairedArray.push(["T", "A"]);
+            break;
+          case "C":
+            pairedArray.push(["C", "G"]);
+            break;
+          case "G":
+            pairedArray.push(["G", "C"]);
+            break;
+        }
+      };
+    
+
+      
