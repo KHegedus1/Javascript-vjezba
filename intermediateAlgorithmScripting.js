cc5c9acef1502bbafc096b06afe7b8f5f8b8a45d
KHegedus1 <kico.kiki@gmail.com>
2022-07-28 15:25:46 +0200

    pig latin

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -54,4 +54,12 @@ function sumAll(arr) {
 
 
 
+  function translatePigLatin(str) {
+    return str
+      .replace(/^[aeiou]\w*/, "$&way")
+      .replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
+  }
+
+
+
   
