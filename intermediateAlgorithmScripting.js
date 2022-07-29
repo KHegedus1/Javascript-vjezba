1bf3df9a0ed686409ba719dbd7ee042f71d8b1ac
KHegedus1 <kico.kiki@gmail.com>
2022-07-29 10:08:55 +0200

    arguments optional

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -261,4 +261,17 @@ function sumAll(arr) {
 
         
 
+        function addTogether() {
+          const [first, second] = arguments;
+          if (typeof(first) !== "number")
+            return undefined;
+          if (arguments.length === 1)
+            return (second) => addTogether(first, second);
+          if (typeof(second) !== "number")
+            return undefined;
+          return first + second;
+        }
+
+        
+
         
