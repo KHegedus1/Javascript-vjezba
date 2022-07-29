48152d97c62c602785bdd29faf9422549d9f4acf
KHegedus1 <kico.kiki@gmail.com>
2022-07-29 10:04:11 +0200

    steamroller vjezba

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -227,4 +227,13 @@ function sumAll(arr) {
 
 
 
+        function steamrollArray(arr) {
+          const flat = [].concat(...arr);
+          return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
+        }
+        
+        steamrollArray([1, [2], [3, [[4]]]]);
+
+
+
         
