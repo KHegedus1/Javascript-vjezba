64d8e996026795363821f714871a63aa4b6a7e18
KHegedus1 <kico.kiki@gmail.com>
2022-07-29 10:03:16 +0200

    drop it vjezba

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -216,4 +216,15 @@ function sumAll(arr) {
 
 
 
+        function dropElements(arr, func) {
+          while (arr.length > 0 && !func(arr[0])) {
+            arr.shift();
+          }
+          return arr;
+        }
+        
+        dropElements([1, 2, 3], function(n) {return n < 3; });
+
+
+
         
