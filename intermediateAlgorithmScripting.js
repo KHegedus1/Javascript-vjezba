812cf68736c05a01979b6ff20b6fd1e4f1eeb1c7
KHegedus1 <kico.kiki@gmail.com>
2022-07-29 10:05:03 +0200

    binary agents

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -236,4 +236,16 @@ function sumAll(arr) {
 
 
 
+        function binaryAgent(str) {
+          var biString = str.split(" ");
+          var uniString = [];
         
+          for (var i = 0; i < biString.length; i++) {
+            uniString.push(String.fromCharCode(parseInt(biString[i], 2)));
+          }
+        
+          return uniString.join("");
+        }
+
+        
+
