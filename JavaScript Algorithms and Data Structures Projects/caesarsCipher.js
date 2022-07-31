2f9a40fc9112578e762c276564f0ffdbe0a252e5
KHegedus1 <kico.kiki@gmail.com>
2022-07-31 18:08:26 +0200

    Tests

--- JavaScript Algorithms and Data Structures Projects/caesarsCipher.js
+++ JavaScript Algorithms and Data Structures Projects/caesarsCipher.js
@@ -9,3 +9,12 @@ Write a function which takes a ROT13 encoded string as input and returns a decod
 All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
 
 */ 
+
+/*
+Tests:
+rot13("SERR PBQR PNZC") should decode to the string FREE CODE CAMP
+Waiting:rot13("SERR CVMMN!") should decode to the string FREE PIZZA!
+Waiting:rot13("SERR YBIR?") should decode to the string FREE LOVE?
+Waiting:rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+*/
+
