f4999940648e01ebc7ffbc7166a1b6082ec6927c
KHegedus1 <kico.kiki@gmail.com>
2022-07-31 18:28:03 +0200

    Rjesenje

--- JavaScript Algorithms and Data Structures Projects/telephoneNumberValidator.js
+++ JavaScript Algorithms and Data Structures Projects/telephoneNumberValidator.js
@@ -51,8 +51,11 @@ telephoneCheck("11 555-555-5555") should return false.
 
 
 
+const regex = /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
+
 function telephoneCheck(str) {
-    return true;
-  }
+  return regex.test(str);   //The test() method tests for a match in a string. If it finds a match, it returns true, otherwise it returns false.
   
-  telephoneCheck("555-555-5555");
+}
+
+telephoneCheck("555-555-5555");
