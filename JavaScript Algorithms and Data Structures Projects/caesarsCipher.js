d7358874b769c5026dedebff53de7ca9eef49d5b
KHegedus1 <kico.kiki@gmail.com>
2022-08-01 10:26:40 +0200

    Update caesarsCipher.js

--- JavaScript Algorithms and Data Structures Projects/caesarsCipher.js
+++ JavaScript Algorithms and Data Structures Projects/caesarsCipher.js
@@ -18,6 +18,8 @@ Waiting:rot13("SERR YBIR?") should decode to the string FREE LOVE?
 Waiting:rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
 */
 
+
+
 function rot13(str) {
     let charCode = 0;
     let niz = [];
@@ -39,4 +41,3 @@ function rot13(str) {
   }
   
   rot13("SERR PBQR PNZC");
-  
